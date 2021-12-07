10300eee5ba5891b4d78d0be300d566bd6d3a480
cottmar <cara.ottmar@gmail.com>
2021-12-06 21:46:15 -0800

    2

--- per-wear/src/NewItem/ItemForm.js
+++ per-wear/src/NewItem/ItemForm.js
@@ -5,42 +5,40 @@ const ItemForm = (props) => {
     const [ itemEntered, setItemEntered ] = useState('');
     const [ amountEntered, setAmountEntered ] = useState('');
 
-    const addItemHandler = (event) => {
+    const itemAddHandler = (event) => {
         setItemEntered(event.target.value);
     }
 
-    const addAmountHandler = (event) => {
-        setAmountEntered(event.target.value);
+    const amountAddHandler = (event) => {
+        setAmountEntered(event.target.handler)
     }
 
-    const onSubmit = (event) => {
+    const onSubmitHandler = (event) => {
         event.preventDefault();
 
         const itemData = {
             item: itemEntered,
             amount: amountEntered
         }
-        
-        console.log(itemData, 'this is the item data')
-
+        console.log(itemData, 'This is the item data')
     }
 
     return (
-            <form onSubmit={onSubmit}>
-                <label>Item: </label>
-                <input 
-                    type='text'
-                    onChange={addItemHandler}
-                    value={itemEntered}
-                />
-                <label>Amount: </label>
-                <input 
-                    type='number'
-                    onChange={addAmountHandler}
-                    value={amountEntered}
-                />
-                <button type='submit'>Add New Item</button>
-            </form>
+        <form onSubmit={onSubmitHandler}>
+            <label>Item: </label>
+            <input 
+                type='text'
+                value={itemEntered}
+                onChange={itemAddHandler}
+            />
+            <label>Amount: </label>
+            <input 
+                type='number'
+                value={amountEntered}
+                onChange={amountAddHandler}
+            />
+            <button type='submit'>Add New Item and Value</button>
+        </form>
     )
 }
 
