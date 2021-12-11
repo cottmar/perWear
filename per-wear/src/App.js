f556df4f5bc3a8b3fe10a10575a22d37039c00b5
cottmar <cara.ottmar@gmail.com>
2021-12-10 21:35:59 -0800

    working on rendering items added

--- per-wear/src/App.js
+++ per-wear/src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
+import React, { useState } from 'react';
 import NewItem from './NewItem/NewItem';
+import ItemsList from './NewItem/ItemsList';
 
-function App() {
+const DUMMY_EXPENSES = [
+  {
+    id: 'i1',
+    item: 'Peloton',
+    amount: 1400
+  },
+  {
+    id: 'i2',
+    item: 'Clutch',
+    amount: 247
+  }
+]
+
+const App = () => {
+  const [ items, setItems ] = useState(DUMMY_EXPENSES);
 
   const addItemHandler = (item) => {
-    console.log(item, 'THE ITEM')
+    setItems(prevItems => {
+      console.log('APP ITEM AND PREV ITEMS', item, prevItems)
+      return [item, ...prevItems]
+    })
+    console.log(item, 'IN APP.JS - THE ITEM')
   }
 
   return (
@@ -12,6 +32,7 @@ function App() {
       <NewItem 
         onAddItem={addItemHandler}
       />
+      <ItemsList item={items}/>
     </div>
   );
 }
